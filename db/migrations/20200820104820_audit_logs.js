29a1dcce4a27c5b71c29df5351c911b9950265e0
sgwicks <samgwicks@gmail.com>
2020-08-20 10:57:38 +0100

    Add migration file for audit_logs2

---
+++ db/migrations/20200820104820_audit_logs.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('audit_logs', logTable => {
+      logTable.increments('log_id');
+      logTable.string('user_id').notNullable();
+      logTable.string('curency_from', 3).notNullable();
+      logTable.string('currency_to', 3).notNullable();
+      logTable.decimal('exchange_rate').notNullable();
+      logTable.decimal('amount_given').notNullable();
+      logTable.decimal('amount_received').notNullable();
+      logTable.datetime('date').defaultTo(knex.fn.now());
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('audit_logs')
+};
