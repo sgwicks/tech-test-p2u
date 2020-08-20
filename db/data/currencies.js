8242ec36bf36176ef5fbc6621359a7e4d9c79bbe
sgwicks <samgwicks@gmail.com>
2020-08-20 10:11:21 +0100

    Create currencies data

---
+++ db/data/currencies.js
@@ -0,0 +1,27 @@
+module.exports = {
+    GBP: {
+        GBP: 1,
+        USD: 0.75,
+        AUD: 1.5,
+        EUR: 0.6
+    },
+    USD: {
+        GBP: 1.33,
+        USD: 1,
+        AUD: 0,
+        EUR: 0
+    },
+    AUD: {
+        GBP: 0.66,
+        USD: 0,
+        AUD: 1,
+        EUR: 0
+    },
+    EUR: {
+        GBP: 1.5,
+        USD: 0,
+        AUD: 0,
+        EUR: 1
+
+    }
+}
